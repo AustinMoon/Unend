3427949d104ed89c791ef2011a1a38905207eca9
abdulrahman aldaghfaq <abdul@abdul.local>
2016-06-18 15:48:21 -0400

    no message

--- quick_corrections/php/signup.php
+++ quick_corrections/php/signup.php
@@ -74,19 +74,4 @@
        
                 </div></div>
         </div></div>
-
-    <!-- jQuery -->
-    <script src="../css/bower_components/jquery/dist/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../css/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../css/bower_components/metisMenu/dist/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../css/dist/js/sb-admin-2.js"></script>
-
-</body>
-
-</html>
+<?php include '../html/footer.html';?>
