0055704c899d478e92796e7d791686fa51c8a888
abm90 <moon@Austins-MacBook-Pro.local>
2016-06-23 19:05:28 -0400

    no message

--- new Codeigniter/application/views/header.php
+++ new Codeigniter/application/views/header.php
@@ -11,10 +11,30 @@
 	<meta name="author" content="">
 
 	<!-- css -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link href="../css/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/font-awesome.css" rel="stylesheet">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <!-- MetisMenu CSS -->
+    <link href="../css/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Social Buttons CSS -->
+    <link href="../css/bower_components/bootstrap-social/bootstrap-social.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../css/dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../css/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <script src="../css/bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../css/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../css/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../css/dist/js/sb-admin-2.js"></script>
 
 
 	<!--[if lt IE 9]>
@@ -25,7 +45,7 @@
 <body>
 
 	<header id="site-header">
-		<nav class="navbar navbar-default" role="navigation">
+		<nav class="navbar navbar-default navbar-static-top" role="navigation">
 			<div class="container-fluid">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -34,15 +54,65 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="<?= base_url() ?>">Site title</a>
+                <a class="navbar-brand" href="index.html"><img src="../img/qc_logo.png" style="width:500px;  display:inline; float:left"></a>
 				</div>
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav navbar-right">
 						<?php if (isset($_SESSION['username']) && $_SESSION['logged_in'] === true) : ?>
-							<li><a href="<?= base_url('logout') ?>">Logout</a></li>
+						<li class="dropdown">
+	                   	 <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+	                        <i class="fa fa-bell fa-fw"></i>  <i class="fa fa-caret-down"></i>
+	                    </a>
+	                    <ul class="dropdown-menu dropdown-alerts">
+	                        <li>
+	                            <a href="#">
+	                                <div>
+	                                    <i class="fa fa-comment fa-fw"></i> New Update
+	                                    <span class="pull-right text-muted small">4 minutes ago</span>
+	                                </div>
+	                            </a>
+	                        </li>
+	                        <li class="divider"></li>
+	                        <li>
+	                            <a href="#">
+	                                <div>
+	                                    <i class="fa fa-envelope fa-fw"></i> Text submitted
+	                                    <span class="pull-right text-muted small">4 minutes ago</span>
+	                                </div>
+	                            </a>
+	                        </li>
+	                       	                        
+	                        <li class="divider"></li>
+	                        <li>
+	                            <a class="text-center" href="#">
+	                                <strong>See All Alerts</strong>
+	                                <i class="fa fa-angle-right"></i>
+	                            </a>
+	                        </li>
+	                    </ul>
+                    <!-- /.dropdown-alerts -->
+                </li>
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="<?= base_url('logout') ?>"><i class="fa fa-sign-out fa-fw"></i> Sign Out</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
 						<?php else : ?>
-							<li><a href="<?= base_url('register') ?>">Register</a></li>
-							<li><a href="<?= base_url('login') ?>">Login</a></li>
+							<li ><a class=""  href="#">Why QuickCorrections</a></li>                  
+                        	<li ><a class=""  href="#">Price</a></li>
+                            <li><a href="<?= base_url('register') ?>">Sign Up</a></li>
+							<li><a href="<?= base_url('login') ?>">Sign In</a></li>
 						<?php endif; ?>
 					</ul>
 				</div><!-- .navbar-collapse -->
