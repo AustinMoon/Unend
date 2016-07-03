326cdd652f8cece9f68f9da81db50a83bfb5fe6a
abdulrahman aldaghfaq <abdul@abdul.local>
2016-07-02 22:12:42 -0400

    tutor page

---
+++ new Codeigniter/application/controllers/Tutor.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tutor extends CI_Controller {
+    
+    //runs before every function
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->library(array('ion_auth','form_validation'));
+		$this->load->helper(array('url','language'));
+
+		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+
+		$this->lang->load('auth');
+	}
+    
+    function index(){
+        
+        $this->load->model('tutor_model');
+        $data = new stdClass();
+        $data->content= $this->tutor_model->open_requests();
+        $this->load->view('tutor/index',$data);
+    }
+    
+    
+    
+}
---
+++ new Codeigniter/application/models/Tutor_model.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * User_model class.
+ * 
+ * @extends CI_Model
+ */
+class Tutor_model extends CI_Model {
+
+	/**
+	 * __construct function.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
+		
+		parent::__construct();
+		$this->load->database();
+        
+	}
+    
+    public function open_requests(){
+        
+        $this->db->where('is_assigned', 'false');
+        $query = $this->db->get('sentence_correct');
+        return $query->row;
+		
+        
+    }
+        
+    
+}
+	
