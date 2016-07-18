901448c86e81cb108a9b92c3ac3f3f6015cb4ba6
sherdil <khawajas@vcu.edu>
2016-07-18 19:03:25 -0400

    done...

--- new Codeigniter/application/views/user/edited_proof.php
+++ new Codeigniter/application/views/user/edited_proof.php
@@ -48,7 +48,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header text-center" style="font-family:avenir">PRONUNCIATION</h1>
+                    <h1 class="page-header text-center" style="font-family:avenir">PROOFREAD</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
