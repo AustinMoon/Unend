833d88ec71307817d328cde8f5f0ffbdaaf83edb
abdulrahman aldaghfaq <abdul@abdul.local>
2016-07-19 16:01:17 -0400

    show and edit user points + paging

--- new Codeigniter/application/controllers/Admin.php
+++ new Codeigniter/application/controllers/Admin.php
@@ -24,15 +24,41 @@ class Admin extends CI_Controller {
 			// redirect them to the login page
 			redirect('auth/login', 'refresh');
 		}
-        {$data = new stdClass();
-            $user = $this->ion_auth->user()->row();
-            $data->points= $user->points;
-            $this->load->view('html/header',$data);}
-        
-        $this->load->model('tutor_model');
         $data = new stdClass();
+        $this->load->library('pagination');
+        $config['base_url'] = 'http://quickcorrections.com/qc/login3/admin/requests/';
+        $config['total_rows'] = $this->db->get('sentence_correct')->num_rows();
+        $config['per_page'] = 10;
+        $config['num_links'] = 3;
+        $config['full_tag_open'] = '<ul class="pagination">';
+        $config['full_tag_close'] = '</ul><!--pagination-->';
+        $config['first_link'] = '&laquo; First';
+        $config['first_tag_open'] = '<li class="prev page">';
+        $config['first_tag_close'] = '</li>';
+        $config['last_link'] = 'Last &raquo;';
+        $config['last_tag_open'] = '<li class="next page">';
+        $config['last_tag_close'] = '</li>';
+        $config['next_link'] = 'Next &rarr;';
+        $config['next_tag_open'] = '<li class="next page">';
+        $config['next_tag_close'] = '</li>';
+        $config['prev_link'] = '&larr; Previous';
+        $config['prev_tag_open'] = '<li class="prev page">';
+        $config['prev_tag_close'] = '</li>';
+        $config['cur_tag_open'] = '<li class="active"><a href="">';
+        $config['cur_tag_close'] = '</a></li>';
+        $config['num_tag_open'] = '<li class="page">';
+        $config['num_tag_close'] = '</li>';
+// $config['display_pages'] = FALSE;
+// 
+$config['anchor_class'] = 'follow_link';
+        $data->all_requests=$this->db->get('sentence_correct',$config['per_page'],$this->uri->segment(5));
+        $this->pagination->initialize($config);
         $user = $this->ion_auth->user()->row();
-        $data->all_requests= $this->tutor_model->all_requests();
+        $data->points= $user->points;
+        $this->load->view('html/header',$data);
+        $this->load->model('tutor_model');
+        $user = $this->ion_auth->user()->row();
+        //$data->all_requests= $this->tutor_model->all_requests();
         $this->load->view('admin/requests',$data);
         $this->load->view('html/footer.html');
         
--- new Codeigniter/application/views/auth/index.php
+++ new Codeigniter/application/views/auth/index.php
@@ -21,6 +21,7 @@
 		<th><?php echo lang('index_email_th');?></th>
 		<th><?php echo lang('index_groups_th');?></th>
 		<th><?php echo lang('index_status_th');?></th>
+        <th><?php echo 'Points';?></th>
 		<th><?php echo lang('index_action_th');?></th>
 	</tr>
   </thead>
@@ -37,6 +38,7 @@
                 <?php endforeach?>
 			</td>
 			<td><?php echo ($user->active) ? anchor("auth/deactivate/".$user->id, lang('index_active_link')) : anchor("auth/activate/". $user->id, lang('index_inactive_link'));?></td>
+            <td><?= $user->points; ?></td>
 			<td><?php echo anchor("auth/edit_user/".$user->id, 'Edit') ;?></td>
 		</tr>
                
