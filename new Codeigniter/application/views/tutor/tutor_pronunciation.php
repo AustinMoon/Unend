09b4437a4c238e05b72d4d6c2573bc442aea991e
sherdil <khawajas@vcu.edu>
2016-07-20 04:17:19 -0400

    tutor instrucion done...

--- new Codeigniter/application/views/tutor/tutor_pronunciation.php
+++ new Codeigniter/application/views/tutor/tutor_pronunciation.php
@@ -58,19 +58,24 @@
             <div class= "row"> 
                 <div class="form-group col-md-8">
                         <!-- <h4 ><i> <?php echo $error;?> </i></h4>-->
-
-                     <iframe src="http://vocaroo.com/?minimal" width="525" height="450" frameborder="0"></iframe><br>Powered by <a href="http://vocaroo.com" title="Voice Recorder">Vocaroo Voice Recorder</a>
+                    <h4><i>Step 1 - Please download the your client's recorded voice. </i></h4>
+                                        
+                    <h4><i>Step 2 - Record your feedback.</i></h4>
                     
-                        <h4 ><i>Upload the revision</i></h4>
+                    <iframe src="http://vocaroo.com/?minimal" width="525" height="450" frameborder="0"></iframe><br>Powered by <a href="http://vocaroo.com" title="Voice Recorder">Vocaroo Voice Recorder</a>
+                    
+                    <h4><i>Step 3 - Upload your feedback.</i></h4>
                        
 
-                        <div class="panel panel-red">
+                    <div class="panel panel-red">
 
                         <input style="display:inline" type = "file" name = "userfile" size = "20" /> 
                         
-                        </div> 
-                    </div>
+                    </div> 
                 </div>
+            </div>
+            
+            <h4><i>Step 4 - If you are having technical issues, please type in your feedback in the textbox below.</i></h4>
             <textarea class="form-control"  id="text" rows="5"placeholder="Type Sentence here..." name="sentence" maxlength="750" autofocus></textarea>
             
             <button href="" class="btn btn-danger">Submit</button>
