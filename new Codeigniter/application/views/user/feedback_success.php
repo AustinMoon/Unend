0d9e706456892cebe0b8f8c0632d79112db0d501
abm90 <moon@Austins-MBP.fios-router.home>
2016-07-27 16:52:07 -0400

    feedback success

---
+++ new Codeigniter/application/views/user/feedback_success.php
@@ -0,0 +1,103 @@
+
+
+      
+       <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        
+                        <li>
+                            <a ><i class=""></i> <h5 style="margin-top:-2px; margin-bottom:-5px">Services</h5></a>
+                        </li>
+                                
+                                <li>
+                                    <a href="<?= base_url('/user/sen_correct_student') ?>"><h5 style="margin-top:-2px; margin-bottom:-5px">&nbsp;-&nbsp;&nbsp;&nbsp;Sentence Correction</h5></a>
+                                </li>
+                                <li>
+                                    <a href="<?= base_url('auth/english_question') ?>"><h5 style="margin-top:-2px; margin-bottom:-5px">&nbsp;-&nbsp;&nbsp;&nbsp;Question about English</h5></a>
+                                </li>
+                                <li>
+                                    <a href="http://quickcorrections.com/qc/login3/auth/pronunciation"><h5 style="margin-top:-2px; margin-bottom:-5px">&nbsp;-&nbsp;&nbsp;&nbsp;Pronunciation</h5></a>
+                                </li>
+                                <li>
+                                    <a href="<?= base_url('auth/proofread') ?>"> <h5 style="margin-top:-2px; margin-bottom:-5px">&nbsp;-&nbsp;&nbsp;&nbsp;Proofreading</h5></a>
+                                </li>
+                               
+                            <!-- /.nav-second-level -->
+                        
+                       
+                        <li>
+                            <a href="http://quickcorrections.com/qc/login3/auth/change_password"><i class=""></i> <h5 style="margin-top:-2px; margin-bottom:-5px">Information</h5></a>
+                            
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="http://quickcorrections.com/qc/login3/user/userpage"><i class=""></i> <h5 style="margin-top:-2px; margin-bottom:-5px">Your Answer</h5></a>
+                        </li>
+                        <li>
+                            <a href="http://quickcorrections.com/qc/login3/user/payment"><i class=""></i><h5 style="margin-top:-2px; margin-bottom:-5px"> Buy Point</h5></a>
+                        </li>
+                    
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+        <!-- Page Content -->
+        <div id= "page-wrapper"class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header text-center" style="font-family:avenir">THANK YOU!</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+
+                <div class= "row">
+                    
+                <div class="col-lg-6 col-lg-offset-3">
+                    <div class="panel panel-red">
+                        <div class="panel-heading text-center">
+                            <h2>
+                           Thank you for your feedback!<br/>
+                           <br/>
+                           Please have a great day!
+                            
+                            </h2>
+                        </div>
+                      
+                    </div>
+                    <a href="<?= base_url('user') ?>"><button class="btn btn-danger col-lg-offset-4">Go to Main Page</button></a>
+                    <!-- /.col-lg-4 -->
+                    
+                </div>
+                    
+              </div>
+
+               
+
+
+                
+                <!-- /.row -->
+        </div>
+            <!-- /.container-fluid -->
+        
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="../css/bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../css/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../css/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../css/dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
