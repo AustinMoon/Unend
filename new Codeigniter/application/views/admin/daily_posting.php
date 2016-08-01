88432d8362be197f2fedb780fe98fbd244a37b7e
abm90 <moon@Austins-MacBook-Pro.local>
2016-08-01 17:06:22 -0400

    whole

---
+++ new Codeigniter/application/views/admin/daily_posting.php
@@ -0,0 +1,32 @@
+       <div class="container-fluid">
+            <div class="row" >
+                 <div class="col-lg-12 page-header text-center">
+                    <h1>Posting</h1>
+                </div>
+            </div>
+            
+            <div class="row text-center" >
+                <div class="col-md-8 col-md-offset-2" >
+                    <h4 ><i>Title </i></h4>
+                    <textarea class="form-control"  id="word_count" rows="1" placeholder="Type here..." name="sentence" maxlength="150" autofocus></textarea>
+
+                    <h4 ><i>Type Daily English Tip below </i></h4>
+                    <textarea class="form-control"  id="word_count" rows="5" placeholder="Type here..." name="sentence" maxlength="750"></textarea>
+        <!-- /#page-wrapper -->
+                </div>
+            </div>
+         </div>
+    
+     <script src="../css/bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../css/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../css/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../css/dist/js/sb-admin-2.js"></script>
+
+</body>
+</html>
